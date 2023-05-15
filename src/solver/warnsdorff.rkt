5c85e25967a9b890a7dcdb2ade8daf662ac8bc96
Vivallo04 <andresvivallo4@gmail.com>
2023-05-14 19:46:12 -0600

    Add improve notes

--- src/solver/warnsdorff.rkt
+++ src/solver/warnsdorff.rkt
@@ -19,14 +19,21 @@
                   (cons (- x 1) (+ y 2)))))
   
   (define (warnsdorff x y path)
+
+    ; remove variable
+    ; store sorted moves in a variable
     (let* ((moves (next-moves x y path))
            (sorted-moves (sort moves (lambda (a b) (<= (length (next-moves (car a) (cdr a) path))
                                                        (length (next-moves (car b) (cdr b) path)))))))
+      
+      ; shoud be a cond
       (if (null? sorted-moves)
           path
           (let ((next-move (car sorted-moves)))
             (warnsdorff (car next-move) (cdr next-move) (cons next-move path))))))
 
+  ; store start-x and start-y in variables
+  
   (let* ((start-x 0)
          (start-y 0)
          (path (list (cons start-x start-y))))

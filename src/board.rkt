0ab45fff82bc7cb38fe7685ef2aa5581330a6b38
Vivallo04 <andresvivallo4@gmail.com>
2023-05-14 19:25:34 -0600

    Adsd base gui canvas

--- src/board.rkt
+++ src/board.rkt
@@ -1,7 +1,27 @@
-#lang racket
-(require racket/gui/base)
+#lang racket/gui
 
+(define board-width 8)
+(define board-height 8)
+(define cell-size 50)
 
+(define (draw-board canvas)
+  (let loop ((x 0) (y 0) (color #f))
+    (cond ((>= x board-width))
+          ((>= y board-height)
+           (loop (add1 x) 0 (not color)))
+          (else
+           (send canvas set-brush (if color "tan" "white") 'solid)
+           (send canvas draw-rectangle (* x cell-size) (* y cell-size) cell-size cell-size #t)
+           (loop x (add1 y) (not color))))))
+
+(define frame (new frame% [label "Chess Board"] [width (* board-width cell-size)] [height (* board-height cell-size)]))
+(define canvas (new canvas% [parent frame]
+                            [style '(border)]
+                            [min-width (* board-width cell-size)]
+                            [min-height (* board-height cell-size)]
+                            [paint-callback (lambda (canvas dc) (draw-board canvas))]))
+
+(send frame show #t)
 
 (define (knight-tour n)
   (define (valid-position? x y path)

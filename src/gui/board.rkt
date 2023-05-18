d5e72dae75b6aab2cd254914d43981392ca5f6ed
Vivallo04 <andresvivallo4@gmail.com>
2023-05-17 23:42:25 -0600

    Base racket GUI

--- src/gui/board.rkt
+++ src/gui/board.rkt
@@ -1,24 +1,18 @@
 #lang racket/gui
 
-(define board-width 8)
-(define board-height 8)
-(define cell-size 50)
-
-(define (draw-board canvas)
-  (let loop ((x 0) (y 0) (color #f))
-    (cond ((>= x board-width))
-          ((>= y board-height)
-           (loop (add1 x) 0 (not color)))
-          (else
-           (send canvas set-brush (if color "tan" "white") 'solid)
-           (send canvas draw-rectangle (* x cell-size) (* y cell-size) cell-size cell-size #t)
-           (loop x (add1 y) (not color))))))
-
-(define frame (new frame% [label "Chess Board"] [width (* board-width cell-size)] [height (* board-height cell-size)]))
-(define canvas (new canvas% [parent frame]
-                            [style '(border)]
-                            [min-width (* board-width cell-size)]
-                            [min-height (* board-height cell-size)]
-                            [paint-callback (lambda (canvas dc) (draw-board canvas))]))
-
+; Make a frame by instantiating the frame% class
+(define frame (new frame% [label "The Knight's tour Problem"] [width 600] [height 600]))
+ 
+; Make a static text message in the frame
+(define msg (new message% [parent frame]
+                          [label "No events so far..."]))
+ 
+; Make a button in the frame
+(new button% [parent frame]
+             [label "Click Me"]
+             ; Callback procedure for a button click:
+             [callback (lambda (button event)
+                         (send msg set-label "Button click"))])
+ 
+; Show the frame by calling its show method
 (send frame show #t)

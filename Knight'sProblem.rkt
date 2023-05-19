94bdf0116c0d9425210976465b5a2270cbd0d043
Noemí Vargas <mimi2002@estudiantec.cr>
2023-05-18 23:16:59 -0600

    Solution for PDC-Todas

--- Knight'sProblem.rkt
+++ Knight'sProblem.rkt
@@ -25,15 +25,12 @@
   (cond ((null? (sort (next-moves x y path) (lambda (a b)
                                               (<= (length (next-moves (car a) (cdr a) path))
                                                   (length (next-moves (car b) (cdr b) path)))))) path)
-                                                      
         (else (warnsdorff (car (car (sort (next-moves x y path) (lambda (a b)
                                                                      (<= (length (next-moves (car a) (cdr a) path))
                                                                          (length (next-moves (car b) (cdr b) path)))))))
-
                           (cdr (car (sort (next-moves x y path) (lambda (a b)
                                                                      (<= (length (next-moves (car a) (cdr a) path))
                                                                          (length (next-moves (car b) (cdr b) path)))))))
-
                           (cons (car (sort (next-moves x y path) (lambda (a b)
                                                                   (<= (length (next-moves (car a) (cdr a) path))
                                                                       (length (next-moves (car b) (cdr b) path)))))) path)))))
@@ -42,7 +39,31 @@
 
 
 
-(PDC-Sol 8 '(1 . 0))
+;(PDC-Sol 8 '(1 . 0))
+
+; Returns 5 solutions to the problem
+; Parameters: the number of boxes and a list with the starting position
+; Output: a list with all the boxes visited
+(define (PDC-Todas n initial-position num-solutions)
+  (define (PDC-Sol-Wrapper position)
+    (reverse (cdr (PDC-Sol n position))))
+
+  (define (generate-solutions i current-position solutions)
+    (cond
+      ((or (= i num-solutions) (null? current-position))
+       solutions)
+      (else
+        (define solution (PDC-Sol-Wrapper current-position))
+        (generate-solutions (+ i 1) (list-ref solution 0) (cons solution solutions)))))
+
+  (define solutions (generate-solutions 0 initial-position '()))
+
+  (define unique-solutions (remove-duplicates solutions))
+  (cond
+    ((>= (length unique-solutions) 5) (take unique-solutions 5))
+    (else unique-solutions)))
+
+(PDC-Todas 3 '(0 . 0) 5)
 
 
 

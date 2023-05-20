c928b5efbe0bfbceca01533a30aff1b3aa5a76d4
Vivallo04 <andresvivallo4@gmail.com>
2023-05-19 23:18:02 -0600

    Remove unnecessary functions

--- src/gui/board.rkt
+++ src/gui/board.rkt
@@ -1,37 +1,5 @@
 #lang racket/gui
 
-(define chess-piece-snip-class
-  (make-object
-   (class snip-class%
-     (super-new)
-     (send this set-classname "chess-piece-snip"))))
-
-(send (get-the-snip-class-list) add chess-piece-snip-class)
-
-(define chess-piece%
-  (class snip%
-    (init-field glyph font size)
-    (super-new)
-    (send this set-snipclass chess-piece-snip-class)
-
-    (define/override (get-extent dc x y width height descent space lspace rspace)
-      (when width (set-box! width size))
-      (when height (set-box! height size))
-      (when descent (set-box! descent 0.0))
-      (when space (set-box! space 0.0))
-      (when lspace (set-box! lspace 0.0))
-      (when rspace (set-box! rspace 0.0)))
-
-    (define/override (draw dc x y . other)
-      (send dc set-font font)
-      (send dc set-text-foreground "black")
-      (define-values (glyph-width glyph-height baseline extra-space)
-        (send dc get-text-extent glyph font #t))
-      (let ((ox (/ (- size glyph-width) 2))
-            (oy (/ (- size glyph-height 2))))
-        (send dc draw-text glyph (+ x ox) (+ y oy))))
-    ))
-
 
 (define chess-board%
   (class pasteboard%
@@ -42,13 +10,12 @@
 
 (define board-size 16)
 (define (draw-chess-board dc)
-  (define brush (send the-brush-list find-or-create-brush "gray" 'solid))
+  (define brush (send the-brush-list find-or-create-brush "orange" 'solid))
   (define pen (send the-pen-list find-or-create-pen "black" 1 'transparent))
   (define font (send the-font-list find-or-create-font 8 'default 'normal 'normal))
   (define-values (dc-width dc-height) (send dc get-size))
   (define cell-width (/ dc-width board-size))
   (define cell-height (/ dc-height board-size))
-  (define margin 3)
     
   (send dc clear)
   (send dc set-brush brush)
@@ -62,13 +29,17 @@
     (send dc draw-rectangle x y cell-width cell-height)))
 
 
+
 ; Define the tile size
 (define tile-size 60)
-;; The pasteboard% that will hold and manage the chess pieces
+
+; The pasteboard% that will hold and manage the chess pieces
 (define board (new chess-board%))
-;; Toplevel window for our application
+
+; Toplevel window for our application
 (define toplevel (new frame% [label "Chess Board"] [width (*  tile-size board-size)] [height (* tile-size board-size)]))
-;; The canvas which will display the pasteboard contents
+
+; The canvas which will display the pasteboard contents
 (define canvas (new editor-canvas%
                     [parent toplevel]
                     [style '(no-hscroll no-vscroll)]
@@ -76,4 +47,5 @@
                     [vertical-inset 0]
                     [editor board]))
 
+; Show the toplevel window
 (send toplevel show #t)

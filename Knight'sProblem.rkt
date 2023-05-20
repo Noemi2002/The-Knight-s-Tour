5c4bd55045f9ebd31e1f161a3ecc890daf4fd6ae
Noemí Vargas <mimi2002@estudiantec.cr>
2023-05-19 23:37:14 -0600

    PDC-Test function

--- Knight'sProblem.rkt
+++ Knight'sProblem.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+
 ;Returns just a solution to the problem
 ;Parameters: the number of boxes and a list with the starting position
 ;Output: a list with all the boxes visited
@@ -55,15 +56,66 @@
       (else
         (define solution (PDC-Sol-Wrapper current-position))
         (generate-solutions (+ i 1) (list-ref solution 0) (cons solution solutions)))))
-
+  
   (define solutions (generate-solutions 0 initial-position '()))
+
 
   (define unique-solutions (remove-duplicates solutions))
   (cond
     ((>= (length unique-solutions) 5) (take unique-solutions 5))
     (else unique-solutions)))
 
-(PDC-Todas 16 '(0 . 0))
+
+;Verifies if a given structure is a valid solution
+;Parameters: the number of boxes and a list-matrix with the solution
+;Output: a boolean
+(define (PDC-Test n sol)
+  (define (valido? x y sol)
+    (and (>= x 0) (< x n) (>= y 0) (< y n) (not (member (cons x y) sol))))
+
+  (define (adyacentes x y sol)
+    (filter (lambda (p) (valido? (car p) (cdr p) sol))
+            `((,(+ x 2) . ,(if (< y 6) +1 -1))
+              (,(+ x 2) . ,(if (< y 6) -1 +1))
+              (,(+ x 1) . ,(if (< y 7) +2 -2))
+              (,(+ x 1) . ,(if (< y 7) -2 +2))
+              (,(- x 2) . ,(if (< y 2) +1 -1))
+              (,(- x 2) . ,(if (< y 2) -1 +1))
+              (,(- x 1) . ,(if (< y 1) +2 -2))
+              (,(- x 1) . ,(if (< y 1) -2 +2)))))
+
+  (define (backtrack x y visited)
+    (cond
+      ((valido? x y visited)
+       (or (backtrack (car (car (adyacentes x y visited))) (cdr (car (adyacentes x y visited))) (cons (car (adyacentes x y visited)) visited))
+           (backtrack x y (cdr visited))))
+      ((= (length visited) (* n n)) #t)  ; Se visitaron todas las casillas
+      (else #f)))
+
+  (backtrack (caar sol) (cdar sol) (cdr sol)))
+
+
+#|(define solu '((0 . 0) (2 . 1) (0 . 2) (2 . 3) (0 . 4)
+  (2 . 5) (0 . 6) (2 . 7) (1 . 1) (3 . 0)
+  (1 . 3) (3 . 2) (1 . 5) (3 . 4) (1 . 7)
+  (3 . 6) (2 . 2) (0 . 1) (2 . 0) (0 . 3)
+  (2 . 4) (0 . 7) (2 . 6) (1 . 2) (3 . 1)
+  (1 . 4) (3 . 3) (1 . 6) (3 . 5) (2 . 3)
+  (0 . 6) (2 . 5) (0 . 4) (2 . 7) (0 . 0)
+  (2 . 1) (0 . 2) (2 . 3) (0 . 6) (2 . 5)
+  (0 . 4) (2 . 7) (0 . 0) (2 . 1) (0 . 2)
+  (2 . 3) (1 . 1) (3 . 0) (1 . 3) (3 . 2)
+  (1 . 5) (3 . 4) (1 . 7) (3 . 6) (2 . 2)
+  (0 . 1) (2 . 0) (0 . 3) (2 . 4) (0 . 7)
+  (2 . 6) (1 . 2) (3 . 1) (1 . 4) (3 . 3)
+  (1 . 6) (3 . 5) (2 . 3) (0 . 5))) 
+
+(PDC-Test 8 solu)|#
+
+
+;(PDC-Todas 16 '(0 . 0))
+;(PDC-Todas 3 '(0 . 0))
+
 
 
 
